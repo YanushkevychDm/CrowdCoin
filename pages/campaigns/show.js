5a4ec6fc4c1f11e295c6206b215803b345aaa9c1
Dmytro Yanushkevych <onswarm@gmail.com>
2018-06-07 14:13:59 +0300

    get contract property to page, part 2

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../etherium/campaign';
+import web3 from '../../etherium/web3';
+
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -17,10 +20,52 @@ class CampaignShow extends Component {
     };
   }
 
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of manager',
+        description: 'The manager this campaign and can create requests to withdraw money.',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum Contribution (wei)',
+        description: 'You must contribute at least this much wei to become an approver.'
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description: 'A request tried to withdraw money from the contract. Requests must be approved by approvers.'
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description: 'Number of people who have already donated to this campaign.'
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign balance (ether)',
+        description: 'The balance is how much money this campaign has left to spend.'
+      },
+    ];
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
