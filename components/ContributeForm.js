1f4c5c0d74c19900b28f3397b7128e48eb337f64
Dmytro Yanushkevych <onswarm@gmail.com>
2018-06-07 19:17:49 +0300

    contribute button and live reload

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
+import Campaign from '../etherium/campaign';
+import web3 from '../etherium/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
-  render() {
+  state = {
+    value: ''
+  }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const campaign = Campaign(this.props.address);
+
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(this.state.value, 'ether')
+      });
+
+      Router.replaceRoute(`/campaigns/${this.props.address}`)
+    } catch(err) {
+
+    }
+  }
+
+  render() {
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field>
+          <label>Amount of Contribute</label>
+          <Input 
+            value={this.state.value}
+            onChange={event => this.setState({ value: event.target.value })}
+            label="ether" 
+            labelPosition="right" />
+        </Form.Field>
+       
+        <Button primary>Contribute {this.state.value} ether!</Button>
+      </Form>
+      
+    )
   }
 }
 
